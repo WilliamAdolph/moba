ed97f6f6ab7edcda0af340e97027f5881d7f7c51
AdolphWilliam <1208479837@qq.com>
2019-06-20 10:03:29 +0800

    王者荣耀后台-bilibili

---
+++ server/plugins/db.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+  const mongoose = require("mongoose")
+  mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba',{
+    useNewUrlParser:true
+  },function(err) {
+    if(err){
+        console.log('连接失败');
+    }else{
+        console.log("连接成功")
+    }
+})
+}
