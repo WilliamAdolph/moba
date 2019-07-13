5ac8d22e641674fb6fe7b622e4a32a488a1dce18
AdolphWilliam <1208479837@qq.com>
2019-07-13 17:05:50 +0800

    添加了部署阶段的

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ app.set('secret','hwhshenda502')
 
 app.use(require('cors')())
 app.use(express.json())
+app.use('/',express.static(__dirname + '/web'))
+app.use('/admin',express.static(__dirname + '/admin'))
 app.use('/uploads',express.static(__dirname + '/uploads'))
 
 require('./plugins/db')(app)
